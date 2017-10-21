87a64b099ad95aec496a2e17618d86bacbfdbdea
Gires Ntchouayang <s0554182@htw-berlin.de>
2017-10-21 17:58:05 +0200

    maven assembler plugin added to pom.xml App.class --> main() changed: check a as argument given email an return the result

--- EmailChecker/src/main/java/de/htwberlin/ai/kbe/EmailChecker/App.java
+++ EmailChecker/src/main/java/de/htwberlin/ai/kbe/EmailChecker/App.java
@@ -1,13 +1,23 @@
 package de.htwberlin.ai.kbe.EmailChecker;
 
 /**
- * Hello world!
+ * Check the Email Syntax with the external Email Checker
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        if(args.length == 0)
+        {
+        	System.out.println("No Email adress has been given to test");
+        	return;
+        }
+        ExternalEmailSyntaxChecker externalEmailSyntaxChecker = new ExternalEmailSyntaxChecker();
+        boolean check = externalEmailSyntaxChecker.isValidEmailAddress(args[0]);
+        if(check)
+        	System.out.println("The Email Syntax is correct!");
+        else
+        	System.out.println("The Email Syntax is not correct!");
     }
 }
