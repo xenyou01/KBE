1f3ef975a12fb516eb2d88689f0e93ca669da87a
Gires Ntchouayang <s0554182@htw-berlin.de>
2017-11-02 10:21:25 +0100

    test class for PropertyUtility added

---
+++ runMeRunner/src/test/java/de/htwberlin/ai/kbe/runMeRunner/PropertyUtilityTest.java
@@ -0,0 +1,17 @@
+package de.htwberlin.ai.kbe.runMeRunner;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PropertyUtilityTest {
+
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	//TODO
+
+}
