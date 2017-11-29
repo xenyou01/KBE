5aaf81f23afa8065ac1681cd855bd3889c0f2ed6
Gires Ntchouayang <s0554182@htw-berlin.de>
2017-11-29 17:41:40 +0100

    test

--- songWebStore/src/test/java/de/htwBerlin/ai/kbe/songWebStore/SongsStoreServletTest.java
+++ songWebStore/src/test/java/de/htwBerlin/ai/kbe/songWebStore/SongsStoreServletTest.java
@@ -51,34 +51,6 @@ public class SongsStoreServletTest {
     		config.addInitParameter("songFilename", "songs.json");
     }
     
-    /*@SuppressWarnings("unchecked")
-	@Test
-    public void playingWithJackson() throws IOException {
-	    // songs.json and testSongs.json contain songs from this Top-10:
-	    // http://time.com/collection-post/4577404/the-top-10-worst-songs/
-	    
-    	    // Read a JSON file and create song list:
-    		InputStream input = this.getClass().getClassLoader().getResourceAsStream("testSongs.json");
-
-    		List<Song> songList = (List<Song>) objectMapper.readValue(input, new TypeReference<List<Song>>(){});
-	    
-	    	// write a list of objects to a JSON-String with prettyPrinter
-	    	String json = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(songList);
-	    
-	    	// write a list of objects to an outputStream in JSON format
-	    	//File file = new File(this.getClass().getClassLoader().getResource("testSongs.json").getFile());
-	    	//objectMapper.writerWithDefaultPrettyPrinter().writeValue(new FileOutputStream(file), songList);
-    
-	    	// Create a song and write to JSON
-	    	Song song = new Song (null, "titleXX", "artistXX", "albumXX", 1999);
-	    byte[] jsonBytes = objectMapper.writeValueAsBytes(song);
-	    
-	    // Read JSON from byte[] into Object
-	    Song newSong = (Song) objectMapper.readValue(jsonBytes, Song.class);
-	    assertEquals(song.getTitle(), newSong.getTitle());
-	    assertEquals(song.getArtist(), newSong.getArtist());
-    }
-    
     @Test
     public void initShouldLoadSongList() throws ServletException, IOException {
     	servlet.init(config);
@@ -154,13 +126,5 @@ public class SongsStoreServletTest {
     public void doPutShouldReturnForbiddenCode() throws IOException {
     	servlet.doPut(request, response);
     	assertEquals(response.getStatus(), 403);
-    }*/
-    
-    @Test
-    public void play() throws FileNotFoundException, URISyntaxException
-    {
-    	//InputStream input = this.getClass().getClassLoader().getResourceAsStream("testSongs.json");
-    	//System.out.println(this.getClass().getClassLoader().getResource("testSongs.json").toURI());
-    	FileOutputStream fos =new FileOutputStream(new File(this.getClass().getClassLoader().getResource("testSongs.json").getPath()));
     }
 }
