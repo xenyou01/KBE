99da2e28e3de8abdbc2c37abde2a019d90579b0d
stefan_neuberger <s0553849@htw-berlin.de>
2017-12-20 15:29:52 +0100

    Jersey Tests korrektur

--- songsRX/src/test/java/de/htwBerlin/ai/kbe/services/SongWebServiceTest.java
+++ songsRX/src/test/java/de/htwBerlin/ai/kbe/services/SongWebServiceTest.java
@@ -1,17 +1,54 @@
 package de.htwBerlin.ai.kbe.services;
 
+import static org.junit.Assert.*;
+
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
 import org.junit.Assert;
 import org.junit.Test;
 
+import de.htwBerlin.ai.kbe.bean.Song;
+
 public class SongWebServiceTest extends JerseyTest {
 	
 	@Override
 	protected Application configure() {
 		return new ResourceConfig(SongWebService.class);
-	}
+	}	
+	 
+	@Test
+    public void testDeleteWithUnknownSongShouldReturn404(){
+        Response output = target("/songs/777").request().delete();
+        assertEquals("Should return status 404", 404, output.getStatus());
+    }
+	
+	@Test
+    public void testDeleteWithKnownSongShouldReturn204(){
+        Response output = target("/songs/10").request().delete();
+        assertEquals("Should return status 204", 204, output.getStatus());
+    }
+	
+	@Test
+    public void testPutWithUnknownSongIdShouldReturn404(){
+		Song song = new Song();		
+        Response output = target("/songs/777")
+        		.request()
+        		.put(Entity.entity(song, MediaType.APPLICATION_JSON));;
+        assertEquals("Should return status 404", 404, output.getStatus());
+    }
+	
+	@Test
+    public void testPutWithKnownSongIdShouldReturn204(){
+		Song song = new Song();
+		 Response output = target("/songs/10")
+	        		.request()
+	        		.put(Entity.entity(song, MediaType.APPLICATION_JSON));;
+	        assertEquals("Should return status 204", 204, output.getStatus());
+    }
 
 }
